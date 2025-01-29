7db5f9875ad249ef8b13be3c6abd6188e3b49a2a
meow-d <meow_d@tuta.io>
2025-01-29 22:59:53 +0800

    fix: window resize, resolution, mouseleave, image display css

--- static/draw.js
+++ static/draw.js
@@ -1,19 +1,43 @@
 const canvas = document.getElementById("draw-canvas")
-
 const ctx = canvas.getContext("2d")
-ctx.lineWidth = 4
-ctx.lineCap = 'round'
+
+const width = canvas.clientWidth
+const height = canvas.clientHeight
+canvas.width = Math.floor(width * devicePixelRatio)
+canvas.height = Math.floor(height * devicePixelRatio)
+ctx.setTransform(devicePixelRatio, 0, 0, devicePixelRatio, 0, 0)
+
+ctx.lineWidth = 2
+ctx.lineCap = "round"
+ctx.lineJoin = "round"
 
 let prevX = null
 let prevY = null
 let isPainting = false
 
-window.addEventListener("mouseup", (e) => isPainting = false)
 canvas.addEventListener("mousedown", (e) => {
     isPainting = true
     draw(e)
 })
+window.addEventListener("mouseup", () => (isPainting = false))
 canvas.addEventListener("mousemove", draw)
+canvas.addEventListener("mouseenter", (e) => {
+    prevX = e.offsetX
+    prevY = e.offsetY
+})
+
+canvas.addEventListener("touchstart", (e) => {
+    e.preventDefault()
+    isPainting = true
+    draw(e)
+})
+window.addEventListener("touchend", () => {
+    isPainting = false
+})
+canvas.addEventListener("touchmove", (e) => {
+    e.preventDefault()
+    draw(e)
+})
 
 function draw(e) {
     if (prevX == null || prevY == null || !isPainting) {
@@ -34,7 +58,6 @@ function draw(e) {
     prevY = currentY
 }
 
-
 function clearCanvas() {
     isPainting = false
     ctx.clearRect(0, 0, canvas.width, canvas.height)
@@ -50,11 +73,11 @@ function saveImage() {
 
 function submitImage() {
     canvas.toBlob((image) => {
-        const input = document.getElementById("image-input");
-        const file = new File([image], "image.png", { type: "image/png" });
-        const dataTransfer = new DataTransfer();
-        dataTransfer.items.add(file);
-        input.files = dataTransfer.files;
-        document.getElementById("image-form").submit();
+        const input = document.getElementById("image-input")
+        const file = new File([image], "image.png", { type: "image/png" })
+        const dataTransfer = new DataTransfer()
+        dataTransfer.items.add(file)
+        input.files = dataTransfer.files
+        document.getElementById("image-form").submit()
     })
 }
